4678a078dd73c14e51e767d8779d2a92b1e81f08
René Rössler <rene@freshx.de>
2025-09-04 08:40:16 +0200

    better --help

--- src/args.rs
+++ src/args.rs
@@ -8,7 +8,7 @@ pub static ARGS: Lazy<Args> = Lazy::new(Args::parse);
 pub struct Args {
     /// Send email with given name to everyone who should have received it in the past.
     /// Should be used to send a email manually if it was added later.
-    #[arg(short, long)]
+    #[arg(short, long, value_name = "TEMPLATE_NAME")]
     pub send_mail_to_oldies: Option<String>,
 
     /// If set, no emails will be sent, only log what would be done.
